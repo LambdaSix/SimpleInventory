bbd4f20d4da0409b07adb167d7e2886d5338de5a
LambdaSix <LambdaSix@users.noreply.github.com>
2014-05-16 15:52:08 +0100

    A simple volume constrained container.
    It won't add items where the volume or weight would exceed the
    capacity of the container.

---
+++ SimpleInventory/SimpleInventory.Examples/VolumeLimited/VolumeConstrainedContainer.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using SimpleInventory.Examples.Common;
+
+namespace SimpleInventory.Examples.VolumeLimited
+{
+    public class VolumeConstrainedContainer<T> : Item, IContainer<T> where T:IVolumeItem
+    {
+        protected Inventory<T> Container = new Inventory<T>();
+        
+        public float MaxVolume { get; set; }
+        public float MaxWeight { get; set; }
+
+        protected float CurrentVolume { get; set; }
+        protected float CurrentWeight { get; set; }
+
+        public VolumeConstrainedContainer(string name, float volume, float weight, float maxVolume, float maxWeight)
+                : base(name, volume, weight)
+        {
+            MaxVolume = maxVolume;
+            MaxWeight = maxWeight;
+        }
+
+        public bool Add(T item)
+        {
+            // If adding this item would overflow our volume
+            if (MaxVolume < (CurrentVolume + item.Volume))
+            {
+                return false;
+            }
+
+            // If adding this item would overflow our weight capacity
+            if (MaxWeight < (CurrentWeight + item.Weight))
+            {
+                return false;
+            }
+
+            Container.Add(item);
+            CurrentVolume += item.Volume;
+            CurrentWeight += item.Weight;
+            return true;
+        }
+
+        public IEnumerable<IContainer<T>> GetContainers()
+        {
+            return Container.GetContainers();
+        }
+
+        public IEnumerable<IContainer<U>> GetContainers<U>()
+        {
+            return Container.GetContainers<U>();
+        }
+
+        public IEnumerable<T> Flatten()
+        {
+            return Container.Flatten();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Container.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Container.GetEnumerator();
+        }
+    }
+}
