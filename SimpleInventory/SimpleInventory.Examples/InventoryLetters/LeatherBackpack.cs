ff830707e05ca0ce8638c2d35db441cca7b5e743
Nicola <LambdaSix@users.noreply.github.com>
2016-10-13 11:21:47 +0100

    Implement constructor for Leather Backpack (Lettered)

--- SimpleInventory/SimpleInventory.Examples/InventoryLetters/LeatherBackpack.cs
+++ SimpleInventory/SimpleInventory.Examples/InventoryLetters/LeatherBackpack.cs
@@ -4,6 +4,8 @@ namespace SimpleInventory.Examples.InventoryLetters
 {
     public class LeatherBackpack : InventoryLetterBag<Item>
     {
-        
+        /// <inheritdoc />
+        public LeatherBackpack() : base("Leather Backpack (Lettered)", Volumes.Litres(25), Weights.Kilograms(1))
+        { }
     }
 }
